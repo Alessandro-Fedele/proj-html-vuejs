54a3c5ab1cf173d1647af89dd13a831fee35b850
Alessandro-Fedele <al.fedele@outlook.it>
2021-11-22 22:03:04 +0100

    hover card added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,13 @@
   <div class="col-4">
     <div class="card">
       <img :src="imgUrl" :alt="title" />
-      <div class="overlay"></div>
+      <div class="overlay">
+        <div class="position-relative">
+          <img src="@/assets/img/zigzag-white.png" alt="separator" />
+          <h2>{{ title }}</h2>
+          <h3>{{ text }}</h3>
+        </div>
+      </div>
     </div>
   </div>
 </template>
